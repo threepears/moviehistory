227700c405d470d17a95ebad683d3b3dae3fe4a1
Anderson Cook <andersonmcook@gmail.com>
2015-11-19 14:46:45 -0600

    added login-promise and register-promise

---
+++ javascripts/login-promise.js
@@ -0,0 +1,23 @@
+define(["q", "jquery"], function(Q, $) {
+  return function(oldUser) {
+    var deferred = Q.defer();
+
+    $.ajax({ url: "https://originalidea.firebaseio.com/" + oldUser + "/.json",
+        method: "GET",
+        data: JSON.stringify(oldUser) })
+      // XHR was successful
+      .done(function(json_data) {
+        // Now we can resolve the promise and send the data
+        console.log("got data", json_data);
+        deferred.resolve(json_data);
+      })
+      // XHR failed for some reason
+      .fail(function(xhr, status, error) {
+        // Since the call failed, we have to reject the promise
+        deferred.reject(error);
+        console.log("error", error);
+      });
+
+    return deferred.promise;
+  }; //--end return  
+}); //--end define
