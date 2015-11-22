0a8bce29dac1be9ad6bd01553af07d4bfeca2bc9
Anderson Cook <andersonmcook@gmail.com>
2015-11-22 15:48:46 -0600

    click search with no input and you can see all the movies you have saved, including your email address which is stored at the same level. that is bad

--- javascripts/firebase-search.js
+++ javascripts/firebase-search.js
@@ -1,17 +1,18 @@
 define(["jquery", "omdb-ajax"], function($, omdbAjax) {
   return function(uid) {
 
-// click find button in Find Movies Modal
+// click search button in Search Movies Modal
     $("#searchButton").click(function () {
 
-// findMovie is set to value of input field
+// findMovie is set to value of input field (currently not being used)
       var searchMovie = $("#search").val();
       console.log("findmovie initial", searchMovie);
 
-// replace spaces in findMovie and set to lowercase
+// replace spaces in findMovie and set to lowercase (this is code from the Find Movies button and will not work here)
       var correctedMovie = searchMovie.toLowerCase().replace(/ /g, "+");
       console.log("findMovie lowercase and replace", correctedMovie);
 
+// snapshot
       var ref = new Firebase("https://originalidea.firebaseio.com/userprofiles/");
       console.log("ref", ref);
 
@@ -21,26 +22,16 @@ define(["jquery", "omdb-ajax"], function($, omdbAjax) {
         var userRef = snapshot.val();
         console.log("inside userRef", userRef);
 
-
+// currently this also writes the email address that is stored at the same level as the movies.
+// that could be fixed or changed several ways i'm sure
         require(['hbs!../templates/firebase-movie-results'], function (songTemplate) {
           $("#home-page .row").html(songTemplate({movies:userRef}));
         });
-
       });
 
+// nothing happens after this, but something should be written to make that thing you want to happen, happen
 
 
-// // put findMovie into ajax call to omdb ?     
-//       omdbAjax(correctedMovie).then(function (movie) {
-
-//       console.log("movie", movie);
-// // handlebars    
-//       require(['hbs!../templates/omdb-movie-results'], function (songTemplate) {
-//         $("#home-page .row").html(songTemplate(movie));
-//       });
-
-
-//     });
 
     }); //--end click
 
