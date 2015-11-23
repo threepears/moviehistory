e7c4fde0cb5d146bf2d043d68f8d6802b91139f2
Andrew2009 <andrewsagraves@gmail.com>
2015-11-23 15:30:12 -0600

    wip landing page

---
+++ javascripts/landingpage.js
@@ -0,0 +1,11 @@
+define(["jquery"], function($) {
+
+
+}
+
+
+
+//I need to get it to where after a user registers or logs in that the next page displays all the movies they have selected and displays those. 
+//in addition, if its the first time a user has logged in, there is nothing to display.  
+
+
